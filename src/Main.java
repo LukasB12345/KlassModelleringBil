446a60d3967483eb6bb7586a82f2010d46992dab
lukas.bommelin <lukas.bommelin@elev.ga.ntig.se>
2023-10-24 14:59:27 +0200

    förta commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,3 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         new MySystem(); //Gör main-metoden så kort som möjligt genom att här anropa de metoder som behövs med en klass
